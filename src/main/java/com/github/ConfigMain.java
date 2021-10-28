5eb431f8254602673430a76317fac27418a5d166
Maksim_Raspopov <hardlolmaster@bk.ru>
2021-10-28 13:36:12 +0500

    delete commented

--- src/main/java/com/github/ConfigMain.java
+++ src/main/java/com/github/ConfigMain.java
@@ -1,24 +1,17 @@
 package com.github;
 
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RepositoryService;
-import org.camunda.bpm.engine.repository.Deployment;
 
 public class ConfigMain {
     public static void main(String[] args) {
         ProcessEngine processEngine;
-        ProcessEngineConfiguration engineConfiguration;
 
-//        engineConfiguration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResourceDefault();
-//        engineConfiguration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("camunda.cfg.xml");
-
-//        processEngine = engineConfiguration.buildProcessEngine();
         processEngine = ProcessEngines.getDefaultProcessEngine();
 
         RepositoryService repositoryService = processEngine.getRepositoryService();
-        Deployment deploy = repositoryService.createDeployment()
+        repositoryService.createDeployment()
                 .addClasspathResource("diagram_4.bpmn")
                 .name("Process 4")
                 .deployWithResult();
